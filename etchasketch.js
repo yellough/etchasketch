fb51007eda3a1579f9aeb8e42bbefe70d39e857d
yellough <63880543+yellough@users.noreply.github.com>
2023-11-19 06:56:19 -0700

    Added ability to change grid size

--- etchasketch.js
+++ etchasketch.js
@@ -1,20 +1,44 @@
-// create 16 div inside div.container
 
+
+// Add the squares to the grid
 let container = document.querySelector(".container");
-function makeGrid (size) {
-for (i=1; i<=size*size; i++) {
-    let square = document.createElement('div');
-    square.className = "square";
-    container.appendChild(square); 
-}
-}
-
-makeGrid(16);
-
-let box = document.querySelectorAll(".square");
-
-box.forEach(function (elem) {
-    elem.addEventListener("mouseover", () => {
-        elem.classList.add("hover");
+function makeGrid(size) {
+    container.replaceChildren();
+    for (i = 1; i <= size * size; i++) {
+        let square = document.createElement('div');
+        let percent = (100 / size) + "%";
+        square.className = "square";
+        square.style.flexBasis = percent;
+        container.appendChild(square);
+    }
+    // Change square color on mouseover
+    let box = document.querySelectorAll(".square");
+    box.forEach(function (elem) {
+        elem.addEventListener("mouseover", () => {
+            elem.classList.add("hover");
+        })
     })
-})
+}
+
+// Set up default grid
+let size = 16;
+makeGrid(size);
+
+// Get the user size and set up new grid
+function setSize() {
+    size = prompt("How many squares per side of the grid?");
+    makeGrid(size);
+}
+
+let sizeButton = document.querySelector("#sizeButton")
+sizeButton.addEventListener("click", setSize)
+
+
+
+
+
+
+
+
+
+
